ba2b61528b6fed0e96aa0545f800f18bfc8572d2
aadesolaferanmi <aadesolaferanmi@gmail.com>
2024-10-18 17:54:43 +0100

    Add files via upload

---
+++ coding.py
@@ -0,0 +1,1351 @@
+#a=40
+#b=40
+#if a==b:
+# print("you are right")
+ #ans=you are right
+
+#a=20
+#b=10
+#if a>b:
+ # print("very good")
+#ans=very good
+
+#c=5
+#a=2
+#if c<a:
+# print("wrong")
+
+#f=[]
+#f.append(7)
+#f.append(2)
+#print(f)
+#ans=[7,2]
+
+#k=[]
+#k.append(5)
+#k.append(0)
+#k.append(2)
+#k.append(9)
+#k.append(4)
+#print(k)
+#ans=[5,0,2,9,4]``
+#import math
+#c=4
+#b=9
+#z=math. sqrt(c/2)+ math.sqrt(b/2)
+#print(f"the value of {z} is {z:.2f}")
+#ans-3.5355339059327378
+#or
+#ans=3.54
+
+#ass
+#f=98
+#j=98
+#if f==j:
+ #  print("correct")
+#else:
+   #print("wrong")
+#ans=correct
+
+#i=1
+#while i<6:
+ #  print(i)
+  # i +=1
+#else:
+ # print("i is no longer less than 6")
+#ans=i is no longer less than 6
+
+
+#i=1
+#while i<6:
+ #  print(i)
+#i +=1
+#ans=11111111111
+#converting code into an array
+#import math 
+#b=144
+#z=math.sqrt(144)d=math.sqrt(a)
+#print(f"the value of d is {d:.2f}")
+
+#import math
+#b=144
+#l=math.sqrt(144)
+#print(f"the value of z is {z:.2f}")
+#g=[]
+#g.append(d)
+#g.append(z)
+#g.append(l)
+#print(g)
+#import math
+#number1=4
+#number2=144
+#c=math.sqrt(4) + math.sqrt(144)
+#print(c)
+#ans=14.0
+
+#i = 1
+#while i < 6:
+   # if i == 3:
+     #   break
+    #print(i)
+ #   i += 1
+
+#write a program to calculate this following
+#trapezium:
+#area=0.5 (a+b)*h                       
+#a=4
+#b=6
+#h=2
+#m=0.5*(a+b)*h 
+#print(m)
+#ans=10.0
+
+#perimeter=a+b+c+d
+#a=3
+#b=6
+#c=3
+#d=2
+#p=a+b+c+d
+#print(p)
+#ans=14.0
+
+#circle
+#area= 3.141*r*r
+#r=5
+#r=5
+#a=3.141* r*r
+#print(area)
+#ans=78.525
+
+#perimeter:
+#p=3.141*3.141*r
+#r=5
+#o=3.141*3.141*r
+#print(p)
+#ans=49.329405
+
+#triangle
+#perimeter= a+b+c
+#a=5
+#b=5
+#c=4
+#g=a+b+c
+#print(g)
+#ans=14
+#area=b*h
+#b=2
+#h=3
+#s=b*h
+#print(a)
+#ans=6
+#rhombus
+#area=1/2 *d_1*d_2
+#d_1=2
+#d_2=3
+#w=1/2*d_1*d_2
+#print(a)
+#ans=3.0
+
+#perimeter
+#p=4s
+#q=4*4
+#print(p)
+#ans=16
+
+#convert it into an array
+#k=[]
+#k.append(m)
+#k.append(p)
+#k.append(a)
+#k.append(o)
+
+#k.append(g)
+#k.append(s)
+#k.append(w)
+#k.append(q)
+#print(k)
+#ans=[10.0,14,5,49.329405,14,6,3.0,16]
+
+#i=1
+#while i <2:
+ # print(i)
+#ans=11111111111111111111
+
+#h=7
+#while h>8:
+  #print(h)
+
+#n=3
+#while n<4:
+#   print(n)
+
+#v=6
+#b=9
+#m=v*b
+#print(m)
+#ans=54
+
+#d=7
+#while d<7:
+ #   print(d)
+
+#c=9
+#while c<10:
+ #    print(c)
+
+#i=1
+#while i<=6:
+   #print(i)
+
+#fruits=["apple," "banana," "cherry" ]
+#for x in fruits:
+ # print(x)
+
+##for x in "banana":
+ #  print(x)
+
+#for x in "cherry":
+ #print(x)
+
+
+#fruits = ["apple", "banana", "cherry"]
+#for x in fruits:
+ #  if x == "banana":
+  #      break
+   #print(x)
+#ans=apple
+
+#fruits=["apple," "banana," "cherry"]
+#for x in fruits:
+  #     if x =="banana":
+ #        break
+#print(x)
+#ans=apple,banana,cherry
+
+#fruits=["apple," "banana," "cherry"]
+#for x in fruits:
+  #if x =="banana":
+ #     continue
+#print(x)
+#ans=apple,banana,cherry
+
+#i=15
+
+#while i<6:
+ #  if i == 3:
+ #   break
+#print(i)
+#i+=1
+
+
+#students=["samuel" "paul" "dara" "shina" "kudirat" ""]
+#for x in students:
+ #    print(x)
+      
+#for  x in "samuel":
+   #print(x)
+#ans=samuel
+
+#for x in "shina":
+ #   print(x)
+#ans =shina
+
+#for x in "dara":
+  #print(x)
+#ans=dara
+
+#for x in "kudirat":
+   #print(x)
+#ans=kudirat
+
+#for x in "paul":
+    #print(x)
+#ans=paul
+
+#students=["samuel," "paul," "dara," "shina," "kudirat"]
+#for x in students:
+ #   if x == "paul":
+  #   continue
+#print(x)
+#answer=["samuel," "paul," "dara," "shina," "kudirat"]
+
+#fruits = ["apple", "banana", "cherry"]
+#for x in fruits:
+  # if x == "banana":
+ #       break
+#   print(x)
+
+#students = ["samuel", "paul", "dara", "shina", "kudirat"]
+#for x in students:
+#   if x == "kudirat":
+ #          break
+  # print(x)
+#ans=samuel", "paul", "dara", "shina"
+
+#students = ["samuel", "paul", "dara", "shina", "kudirat"]
+#for x in students:
+   #if x == "samuel":
+    #       break
+   #print(x)
+#ans=samuel
+
+#students = ["samuel", "paul", "dara", "shina", "kudirat"]
+#for x in students:
+   #if x == "shina":
+  #         break
+ #  print(x)
+#ans="samuel", "paul", "dara"
+
+#students = ["samuel", "paul", "dara", "shina", "kudirat"]
+#for x in students:
+   #if x == "dara":
+  #         break
+ #  print(x)
+#ans=samuel paul
+
+#number = [56,99,54,89]
+#for x in number:
+ #  print(x)
+#ans=56,99,54,89
+
+#for x in number:
+ #if x==99:
+  #    break
+ #print(x)
+#ans=56
+
+#for x in number:
+   #if x ==54:
+   #      break
+  # print(x)
+#ans=56, 99
+
+#for x in number:
+   #if x ==89:
+    #  break
+   #print(x)
+#ans=56, 99,54
+
+#for h in range(6):
+ #    print(h)
+#ans=0,1,2,3,4,5
+
+ #for d in range(26):
+#print(x)
+#ans=0,1,2,3,4,5,e.t.c
+
+#for x in range(2,30,2,):
+   # print(x)
+  
+   #for g in range(8):
+ #   print(x)
+#ans=0,1,2,3,4,5,6,7
+
+ #   m=[]
+ #m.append(h)
+ #m.append(d)
+ #m.append(x)
+ #m.append(g)
+ #print(m)
+
+#i=0
+#while i<8:
+ #  print(i)
+  # i+=2
+#ans=0000
+
+#k=2
+#while k<10:
+ #print(k)
+ #k+=1
+
+
+#staff_members = [
+ #{"name": "Alice Johnson", "position": "Professor", "department": "Computer Science"},
+#{"name": "Bob Smith", "position": "Lecturer", "department": "Mathematics"},
+#{"name": "Charlie Brown", "position": "Research Scientist", "department": "Physics"}
+#]
+#for staff in staff_members:
+  #   print("Name:", staff   .get("name"))
+#print("Position:", staff.get("position"))
+#print("Department:", staff.get("department"))
+#print("-" * 20) 
+#ans=Name: Alice Johnson
+#Name: Bob Smith
+#Name: Charlie Brown
+#Position: Research Scientist
+#Department: Physics
+#--------------------
+#failed-fwd-i-search: _
+
+ #for student in range(10):
+ # print(student)
+#ans= 1,2,3,4,5,6,7,8,9,
+
+
+  # for ini in range(2,20,3):
+   #print(ini)
+
+
+   # q=[]
+#q.append(k)
+#q.append(staff)
+#q.append(student)
+#q.append(ini)
+#print(q)
+#ans=[8, {'name': 'Charlie Brown', 'position': 'Research Scientist', 'department': 'Physics'}, 10, 18]
+
+#i = 1
+#while i < 10:
+  #print(i)
+
+ #c=[]    
+#c.append(i)
+#print(c) 
+
+#sum = 0
+#i = 1
+#if sum == 0 and i < 6:
+ #   print("The sum is correct.")
+#ans=The sum is correct.
+
+#i=2
+#while i < 10:
+  # print(i)
+ #  i+=2    
+
+#r=0
+#while r < 10:
+ #  print(r)
+  # r+=1
+
+#h=3
+#while h<25:
+   # print(h)
+    #h+=7
+#ans=3,10,17,24
+
+#f=4
+#while f<17:
+ #  print(f)
+  # f+=4
+#ans=4,8,12,16,
+
+#v = 4
+#g = []
+#
+#while v < 12:
+ #   g.append(v)
+  #  v += 2
+#print(g)
+#ans=[4, 6, 8, 10]
+#assignment
+#y=1
+#while y<12:
+#  print(y)
+ # y+=6
+#ans=6
+
+#i=5
+#while i<20:
+ # print(i)
+ # i+=4
+#ans=5,9,13,17
+
+#u=8
+#while u<16:
+  #print(u)
+ # u+=1
+#ans=8,9,10,12,13,14,15
+
+#j=9
+#while j<81:
+  #print(j)
+ # j+=9
+#ans=9,18,27,36,45,54,63,72
+
+#f=2
+#while f<6:
+  #print(f)
+ # f+=4
+#ans=2
+
+#s=[]
+#s.append(y)
+#s.append(i)
+#s.append(u)
+#s.append(j)
+#s.append(f)
+#print(s)
+#ans=[12, 21, 16, 81, 6]
+#account_balance=4
+#for x in range(account_balance): 
+ #  print(x)
+#else:
+ #  print("finally finished")
+
+#account_balance=[8]
+#for x in  (account_balance):
+ #  print(x)
+#while account_balance<5:
+ #  print(account_balance)
+#classwork
+#sister_account_balance=9
+#p=sister_account_balance
+#while p<91:
+    #print(p)
+ #   p+=10
+ #   ans=9,19,29,39,49,59,69,79,89
+
+#daddy_account_balance=4
+#r=daddy_account_balance
+#while r< 20:
+   # print(r)
+ #   r+=2
+#ans=4,6,8,10,12,14,16,18,
+
+#mummy_account_balance=6
+#n=mummy_account_balance
+#while n<36:
+  #print(n)
+ # n+=6
+#ans=6,12,18,24,30,
+
+#niece_account_balance=4
+#t=niece_account_balance
+#while t<24:
+    #print(t)
+ #   t+=4
+#ans=4,8,12,16,20
+
+#nephew_account_balance=2
+#g=nephew_account_balance
+#while g<22:
+    #print(g)
+ #   g+=2
+#ans=2,4,6,8,10,12,14,16,18,20
+
+#my_account_balance=[]
+#my_account_balance.append(p)
+#my_account_balance.append(r)
+#my_account_balance.append(n)
+#my_account_balance.append(t)
+#my_account_balance.append(g)
+
+#print(my_account_balance)
+#ans=[99,20,36,24,22]
+
+#answer=[99,20,36,24,22]
+#for t in answer:
+ # print(t)
+#else:
+  #print  ("finally finished")
+#ans=[99,20,36,24,22]
+ #finally finished
+
+#v = 4
+#g = []
+
+#while v < 12:
+ #   g.append(v)
+  #  v += 2
+   # print(g)
+
+#i=4
+#b=[]
+
+#while i<24:
+ # b.append(i)
+#i+=4
+#print(b)
+
+# Initialize lists to collect sequences
+#sister_sequence = []
+#daddy_sequence = []
+#mummy_sequence = []
+#niece_sequence = []
+#nephew_sequence = []
+
+# Sister account balance sequence
+#sister_account_balance = 9
+#p = sister_account_balance
+#while p < 91:
+  #  sister_sequence.append(p)
+ #   p += 10
+# Output for sister_sequence: [9, 19, 29, 39, 49, 59, 69, 79, 89]
+
+ #Daddy account balance sequence
+#daddy_account_balance = 4
+#r = daddy_account_balance
+#while r < 20:
+  #  daddy_sequence.
+    
+ #   m append(r)
+ #   r += 2
+# Output for daddy_sequence: [4, 6, 8, 10, 12, 14, 16, 18]
+
+# Mummy account balance sequence
+#mummy_account_balance = 6
+#n = mummy_account_balance
+#while n < 36:
+  #  mummy_sequence.append(n)
+ #   n += 6
+# Output for mummy_sequence: [6, 12, 18, 24, 30]
+
+# Niece account balance sequence
+#niece_account_balance = 4
+#t = niece_account_balance
+#while t < 24:
+ #   niece_sequence.append(t)
+ #   t += 4
+# Output for niece_sequence: [4, 8, 12, 16, 20]
+
+# Nephew account balance sequence
+#nephew_account_balance = 2
+#g = nephew_account_balance
+#while g < 22:
+ #  nephew_sequence.append(g)
+#g += 2
+# Output for nephew_sequence: [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+
+# Combine all sequences into one list
+#my_account_balance = []
+#my_account_balance.extend(sister_sequence)
+#my_account_balance.extend(daddy_sequence)
+#my_account_balance.extend(mummy_sequence)
+#my_account_balance.extend(niece_sequence)
+#my_account_balance.extend(nephew_sequence)
+
+#print(my_account_balance)
+#Expected Output: [9, 19, 29, 39, 49, 59, 69, 79, 89, 4, 6, 8, 10, 12, 14, 16, 18, 24, 30, 4, 8, 12, 16, 20, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+
+#b=[]
+#c=[]
+
+#z=4
+#while z<13:
+ #   b.append(z)
+#z+=4
+
+#x=2
+#while x<7:
+ #   c.append(x)
+#x+=2
+
+#m=[]
+#m.extend(b)
+#m.extend(c)
+#print(m)
+#      ASSIGNMENT
+#sister_account_balance=[]
+#daddy_account_balance=[]
+#mummy_account_balance=[]
+#niece_account_balance=[]
+#nephew_account_balance=[]
+
+#v=3
+#while v<9:
+   # sister_account_balance.append(v)
+  #  v+=3
+
+ #   m=8
+#while m<24:
+  #  daddy_account_balance.append(m)
+ #   m+=8
+
+#w=5
+#while w<15:
+  #  mummy_account_balance.append(w)
+ #   w+=5
+
+#h=6
+#while h<18:
+   # niece_account_balance.append(h)
+ #   h+=6
+
+#f=4
+#while f<16:
+ #   nephew_account_balance.append(f)
+  #  f+=4
+
+#c=[]
+#c.extend(sister_account_balance)
+#c.extend(daddy_account_balance)
+#c.extend(mummy_account_balance)
+#c.extend(niece_account_balance)
+#c.extend(nephew_account_balance)
+#print(c)
+#ans=[3, 6, 8, 16, 5, 10, 6, 12, 4, 8, 12]
+
+#answer=[3, 6, 8, 16, 5, 10, 6, 12, 4, 825, 12]
+#for t in answer:
+ #   print(t)
+#else:
+ #   print("finally finished")
+#ans=3, 6, 8, 16, 5, 10, 6, 12, 4, 8, 12
+
+#program to calculate the perimeter of a triangle
+#formula=0.5*base*height
+#h=4
+#b=6
+#a=0.5*b*h
+#print(a)
+#       ASSIGNMENT
+#write a program to calculate the amount of money that the customer will pay
+#snacks_price=200
+#snacks_quantity=2
+#snacks_amount=snacks_price*snacks_quantity
+
+#milo_price=200
+#milo_quantity=2
+#milo_amount=milo_price*milo_quantity
+
+#drinks_price=300
+#drinks_quantity=3
+#drinks_amount=drinks_price*drinks_quantity
+
+#biscuits_price=150
+#biscuits_quantity=2
+#biscuits_amount=biscuits_price*biscuits_quantity
+
+#result=snacks_amount+milo_amount+drinks_amount+biscuits_amount
+#print(result)
+#ans=2000
+
+#write a program to add two number together
+#s=4
+#v=9
+#a=s+v
+#print(a)
+#v=3
+#while v<9:
+    #print(v)
+    #v+=3
+
+#add=["rod," "big," "tasty,"]
+#fruits=["apple," "banana," "cherry"]
+#for x in add:
+  #  for y in fruits:
+ #       print(x)
+#        print(y)
+#ans=rod, big,tasty,
+#ans=apple,banana, cherry
+
+#write a program to print even number from 1 to 10
+#a=2
+#while a<10:
+ #   print(a)
+ #   a+=2
+#ans=2,4,6,8,9
+#write a program to print odd number 1 to 10
+#d=1
+#while d< 10:
+  #print(d)
+  #d+=2
+#ans=1,3,5,7,9
+#write a program to print prime number 1 to 10
+#v=1
+#while v<10:
+ #  print(v)
+ #  v+=2
+#ans=1,3,5,7
+#add all the add numbers from 1 to 10 together
+
+
+
+#sum=2
+#for a in range (1,15):
+  #  if a %2==0:
+   #     sum=+a
+    #    print(sum)
+
+#sum=2
+#for a in range (1,15):
+    #if a %2!=0:
+   #     sum=+a
+ #       print(sum)
+
+#program to add even number together
+#v=2
+#sum=0
+#while v< 15:
+ #sum+=v
+ #sum=sum+v
+#print(sum)
+#v+=2
+    
+#v=1
+#sum=0
+#while v< 15:
+ # sum+=v
+  #print(sum)
+  #v+=2
+#ans=1,4,9,16,25,36,49
+      #ASSIGNMENT
+#v=1
+#sum=0
+#while v< 20:
+  #sum+=v
+ # print(sum)
+#  v+=1
+
+#           assignment
+#a program to add the each number of a total_amount from the first item to the last item using while loop
+#price=200
+#quantity=2
+#total_price_of_snacks=price *quantity
+
+#price=200
+#quantity=2
+#total_price_of_drinks=price*quantity
+
+#price=300
+#quantity=3
+#total_price_of_milo=price*quantity
+
+#price=150
+#quantity=2
+#total_price_of_biscuits=price*quantity
+
+#total_amount=total_price_of_snacks+total_price_of_drinks+total_price_of_milo+total_price_of_biscuits
+
+#a=[]
+#a.append(total_price_of_biscuits)
+#a.append(total_price_of_drinks)
+#a.append(total_price_of_snacks)
+#a.append(total_price_of_milo)
+#print(a)
+#ans=[300,400,400,900]
+
+#ans=[300,400,400,900]
+#sum=0
+#for t in ans:
+#  sum+=t
+#print(sum)
+#answer=2000
+# program to print even number
+#sum=0
+#z=2
+#sum=sum+z
+#while z<10:
+ #   print(sum)
+ #   z+=2
+
+#a program to loop from 1to 10
+#a=1
+#while a<=10:
+  #  print(a)
+ #   a+=1   
+
+#           assignment
+#write a program to print the odd number in a score 
+#z=1
+#while z<20:
+ #   print(z)
+#    z+=2
+#ans=1,3,5,7,9,11,13,15,17,19
+
+#write a program to print the even number in a score
+#c=2
+#while c<20:
+ #     print(c)
+  #    c+=2       
+
+
+#        assignment
+#a program to add 1 to 20 in a score
+#i=1
+#sum=0
+#while i<20:
+   # sum+=i
+ #   i+=1
+  #  print(sum)
+
+#write a program to add the odd number in a gross 
+#write a program to add the even numbers in two dozen
+#and add them to an array
+#                   answer
+#v=1
+#sum=0
+#while v<144:
+  #sum+=v
+ # v+=2
+  #print(sum)
+  
+#c=2
+#sumarise=0
+#while c < 24:
+  #  sumarise+=c
+ #   c+=2
+    #print(sumarise)
+
+#a=[]
+#a.append(sum)
+#a.append(sumarise)
+#add=0
+#for t in a:
+ # add+=t
+#print(add)
+#ans=5316
+
+#write a program to multiply the even number in a score
+#v=2
+#sum=0
+#while v<20:
+    #sum=v+sum
+    #v+=2
+   # print(sum)
+
+
+
+#v=2
+#sum=20
+#while v<20:
+#   sum=v+sum
+#v+=2
+#print(sum)
+
+#a program using for loop to print number from 1 to 10 to
+#a=1
+#for x in a:
+#   a+=1
+ #  print(x)
+ 
+#                  assignment
+#write a program to print out even number in a dozen
+#sis=[]
+#dad=[]
+#mum=[]
+
+#e=2
+#while e<12:
+  #  sis.append(e)
+ #   e+=2
+    
+#write a program to print out odd number in a score
+#o=1
+#while o <20:
+    #dad.append(o)
+    #o+=2
+   
+#write a program using for loop to print out number from 1 to 10
+
+#for number in range (1,11):
+ #   mum.append(number)
+     
+#then put them to an array and add them together into one array 
+#a=[]
+#a.extend(sis)
+#a.extend(dad)
+#a.extend(mum)
+#sum=0
+#for t in a:
+ # sum+=t    
+#  print(sum)
+#ans=185
+
+#i=1
+#mul=1
+#while i <185:
+  # mul*=i
+   #i+=10
+   #print(mul)
+
+  # g=[]
+ #  g.append(mul)
+ #  print(g)
+#declare another variable then compare it with the answer you get with if and else statement
+#d=92
+#if sum <d:
+ #  print("it is not correct")
+#else:
+#   print("it is correct")
+#ans=it is correct
+#write a program to multiply even number in a score
+#i=1
+#multiplication=1
+#while i< 20:
+ #  multiply=i*2
+#   i+=2
+#write a program to multiply odd number from 1 to 15
+#i=1
+#multiply=1
+#while i< 16:
+  #multiply=i*2    
+  #i+=1
+  #print(multiply)
+#ans=2,4,6,8,10,12,14,16,18,20,22,24,26,28,30
+#460             assignment
+ # a man own a sugar company with ten employee write a program to count the days of the employee at work in a month, 
+#if the work is monday to friday and if each employee produce 20 sugar per day how many sugar will all the employee 
+#produce in a month
+#             solution
+#d=[]
+#e=[]
+#f=[]
+#m=[]
+#l=[]
+
+#first_week=7
+#d.append(first_week)
+#first_week+=1
+
+#second_week=7
+#e.append(second_week)
+#second_week+=1
+
+#third_week=7
+#f.append(third_week)
+#third_week+=1
+
+#fourth_week=7
+#m.append(fourth_week)
+#fourth_week+=1
+
+#fifth_week=4
+#l.append(fifth_week)
+#fifth_week+=1
+
+#n=[]
+#n.extend(d)
+#n.extend(e)
+#n.extend(f)
+#n.extend(m)
+#n.extend(l)
+#sum=0
+#for t in n:
+  #  sum+=t
+ #   print(sum)
+#ans=7,14,21,28,32
+#c=1
+#mul=1
+#while c<23:
+  # mul*=c
+ #  c+=1
+#print(mul)
+#ans=1124000727777607680000                      
+#e=[]
+
+#for number in range(1,32):
+   #e.append(number) 
+   #g=[]
+  # g.extend (e)
+ #  print(g)
+#def add_numbers_together(a,b):
+#     return a+b
+#result=add_numbers_together(2,3)
+#print(result)
+#ans=5
+
+
+
+
+
+
+
+#f=20
+#while f >2:
+   # f-=2
+  #  print(f)
+
+
+#            ASSIGNMENT
+
+#d=[]
+#g=[]
+
+#u=100
+#while u> 2:
+  #d.append(u)
+  #u-=2
+#for num in range(99, 0, -2): 
+ #  g.append(num)
+
+#c=[]
+#c.extend(d)
+#c.extend(g)
+#print(c)
+
+
+
+
+
+
+#b=2
+#decrement=1
+#while b<100:
+ #   decrement+=b
+#b+=2
+#print(decrement)
+
+#d=1
+#t=0
+#while d<100:
+ #   t+=d
+#d+=2
+#print(t)
+
+#c=[]
+#c.append(decrement)
+#c.append(t)
+#print(c)
+
+#def check_even(num):
+ #   if num <20:
+  #     return False
+   # if num % 2 ==0:
+    #     return True
+         
+#f=1
+#while f<=20:    
+  #f+=2
+  #print(f)
+#ans=1,2,5,7,9,11,13,15,17,19,
+
+#def check_even(num):
+ #   if num <1:
+  #     return False
+   # if num % 2 ==0:
+    #     return True
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
